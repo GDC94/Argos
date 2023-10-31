885c0acc4705ef89eceef808fe17ba5d54e87b85
GDC94 <germanderbescatoni@gmail.com>
2023-10-31 15:11:53 -0300

    argos/adding firts post mdx to proof

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
 
-import { Inter } from "next/font/google";
+import { GeistSans } from "geist/font";
 
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+
 
 export const metadata: Metadata = {
   title: "GDC - Portfolio",
@@ -13,8 +13,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html className={GeistSans.className} lang="en">
+      <body>{children}</body>
     </html>
   );
 }
